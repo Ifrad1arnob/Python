47eca19e42699537ad391cd052d13c8767595924
Ifrad Hossain Arnob <syedaera403@gmail.com>
2020-08-26 19:13:10 +0600

    Initial commit

---
+++ themepark.py
@@ -0,0 +1,99 @@
+Cost_of_Coach = 550
+Entry_cost = 30
+costofstudents=40
+Noofstudents= input('Input the number of students: ')
+def function(Noofstudents):
+    if Noofstudents <= '45':
+        Entry_cost + Cost_of_Coach
+        CALCULATION=(580/40)
+        message= f'The recommended cost per student would be {CALCULATION}'
+        print(message)
+    elif Noofstudents > '45':
+        print('You cannot take more than 45 students.')
+
+
+
+
+function(Noofstudents)
+
+def task2():
+    student1= input("enter name: ")
+    student1b= input("Has he paid: ")
+    student2= input("enter name: ")
+    student2b= input("Has he paid: ")
+    student3= input("enter name: ")
+    student3b= input("Has he paid: ")
+    student4= input("enter name: ")
+    student4b= input("Has he paid: ")
+    student5= input("enter name: ")
+    student5b= input("Has he paid: ")
+    student6= input("enter name: ")
+    student6b= input("Has he paid: ")
+    student7= input("enter name: ")
+    student7b= input("Has he paid: ")
+    student8= input("enter name: ")
+    student8b= input("Has he paid: ")
+    student9= input("enter name: ")
+    student9b= input("Has he paid: ")
+    student10= input("enter name: ")
+    student10b= input("Has he paid: ")
+    print('The List of students going are given below')
+    if student1b == 'yes':
+        print(student1 + ' is going')
+    else:
+        print(student1+ ' has not paid')
+    if student2b == 'yes':
+        print(student2+ ' is going')
+    else:
+        print(student2+ ' has not paid')
+    if student3b == 'yes':
+        print(student3+ ' is going')
+    else:
+        print(student3+ ' has not paid')
+    if student4b == 'yes':
+        print(student4+ ' is going')
+    else:
+        print(student4+ ' has not paid')
+    if student5b == 'yes':
+        print(student5+ ' is going')
+    else:
+        print(student5+ ' has not paid')
+    if student6b == 'yes':
+        print(student6+ 'is going')
+    else:
+        print(student6+ ' has not paid')
+    if student7b == 'yes':
+        print(student7+ ' is going')
+    else:
+        print(student7+ ' has not paid')
+    if student8b == 'yes':
+        print(student8+ ' is going')
+    else:
+        print(student8+ ' has not paid')
+    if student9b == 'yes':
+        print(student9+ ' is going')
+    else:
+        print(student9+ ' has not paid')
+    if student10b == 'yes':
+        print(student10+ ' is going')
+    else:
+        print(student10+ ' has not paid')
+
+
+task2()
+
+
+    
+    
+    
+
+    
+
+
+  
+
+
+    
+    
+
+    
